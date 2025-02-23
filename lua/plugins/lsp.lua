e79277975a35697bf00b8b8f21bb9f764880c081
Александр Лысак <frigh701@gmail.com>
2025-02-23 21:53:17 +0300

    mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,23 @@
 return {
-	{'neovim/nvim-lspconfig'}
+	{
+		'neovim/nvim-lspconfig',
+		config = function()
+			local lspconfig = require('lspconfig')
+			lspconfig.lua_ls.setup{}
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require('mason-lspconfig').setup(
+				{
+					ensure_installed = {
+						'lua_ls',
+						'rust_analyzer',
+						'gopls',
+					}
+				}
+			)
+		end
+	}
 }
